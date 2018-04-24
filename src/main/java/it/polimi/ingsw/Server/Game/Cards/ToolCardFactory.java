6e3731d6e2bf93a310ab0c73915d7191987ff53a
MattCrich <matthias.crichlow@gmail.com>
2018-04-24 21:45:22 +0200

    Added Tool Card Factory extends Abstract Card Factory

---
+++ src/main/java/it/polimi/ingsw/Server/Game/Cards/ToolCardFactory.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.Server.Game.Cards;
+
+import java.util.ArrayList;
+
+public class ToolCardFactory {
+
+    private String filname;
+
+    public ToolCardFactory(String filname) {
+
+        this.filname = filname;
+
+    }
+
+    public String getFilename() {
+        return filname;
+    }
+
+
+    public Drawable getCard(ArrayList<String> pattern) {
+
+        return new ToolCard(pattern);
+    }
+}
+
