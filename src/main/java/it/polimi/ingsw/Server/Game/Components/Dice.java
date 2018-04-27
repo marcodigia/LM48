2237fe539caea0855e115b43a5d89f407ad9fe06
Fabiodallerive <37809568+fabiodallerive@users.noreply.github.com>
2018-04-27 16:50:37 +0200

    Add some tests DiceBag Add tests DraftedPool Add some test Dice Introduce toString() dice

--- src/main/java/it/polimi/ingsw/Server/Game/Components/Dice.java
+++ src/main/java/it/polimi/ingsw/Server/Game/Components/Dice.java
@@ -4,15 +4,17 @@ import it.polimi.ingsw.Server.Game.Utility.DiceColor;
 
 import java.util.Random;
 
-public class Dice {
+public class Dice implements Cloneable{
 
     private DiceColor color;
     private String value;
+    private Random random;
 
     public Dice(DiceColor color , String value ) {
 
         this.color = color;
         this.value = value;
+        random = new Random();
 
     }
 
@@ -24,10 +26,7 @@ public class Dice {
         return value;
     }
 
-
-
     public void reroll() {
-        Random random = new Random();
         int i = random.nextInt(6) + 1;
         value = Integer.toString(i);
     }
@@ -41,6 +40,20 @@ public class Dice {
                     && ((Dice) obj).getValue().equals( value );
 
         return super.equals(obj);
+    }
+
+    @Override
+    public String toString(){
+        return getDiceColor().getColor() + getValue();
+    }
+
+    public Dice cloneIt(){
+        try {
+            return (Dice) clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
 
---
+++ src/test/java/it/polimi/ingsw/Server/Game/Components/MatrixTest.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.Server.Game.Components;
+
+import it.polimi.ingsw.Server.Game.Cards.CardsComponents.Matrix;
+import it.polimi.ingsw.Server.Game.Utility.DiceColor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatrixTest {
+
+    private Matrix matrix;
+    private ArrayList<String> pattern;
+    private Dice dice;
+
+    @BeforeEach
+    void setup(){
+        pattern = new ArrayList<String>();
+        pattern.add("Y");   //0
+        pattern.add("0");   //1
+        pattern.add("6");   //2...
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("1");
+        pattern.add("5");
+        pattern.add("0");
+        pattern.add("2");
+        pattern.add("3");
+        pattern.add("Y");
+        pattern.add("R");
+        pattern.add("P");
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("4");
+        pattern.add("3");
+        pattern.add("R");   //19
+
+        matrix = new Matrix(5,4);
+        matrix.initialize();
+        matrix.initialize_restricions(pattern);
+
+        dice = new Dice(DiceColor.YELLOW,"2");
+    }
+
+    //@Test
+    void placeWrongDice(){
+        assertFalse(matrix.setDice(dice,0,false,false,false));
+    }
+
+    @Test
+    void placeCorrectDice(){
+        assertTrue(matrix.setDice(dice, 3,false,false,false));
+    }
+
+    @Test
+    void removeDice(){
+        assertTrue(matrix.setDice(dice, 3,false,false,false));
+        assertTrue(matrix.removeDice(3));
+    }
+}
