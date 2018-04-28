80cb6751713f2ae2705542eab11f65c52150ed96
MattCrich <matthias.crichlow@gmail.com>
2018-04-28 20:16:21 +0200

    added an idea to make the Game controller more readable

--- src/main/java/it/polimi/ingsw/Client/GUI/ControllerGame2.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/ControllerGame2.java
@@ -16,12 +16,7 @@ import static it.polimi.ingsw.Client.GUI.ControllerLogin.user;
 public class ControllerGame2 implements Initializable {
 
     private static Label draftToDisable;
-    public Label cellp400, cellp401, cellp402, cellp403, cellp404,
-            cellp410, cellp411, cellp412, cellp413, cellp414,
-            cellp420, cellp421, cellp422, cellp423, cellp424,
-            cellp430, cellp431, cellp432, cellp433, cellp434,
-            draft1, draft2, draft3, draft4, draft5, draft6, draft7, draft8, draft9,
-            p4;
+    public Label p4;
     private ArrayList<Label> cells;
     private String buffer;
     private boolean put = false;
@@ -30,26 +25,6 @@ public class ControllerGame2 implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("loaded");
         p4.setText(user);
-        cells.add(cellp401);
-        cells.add(cellp402);
-        cells.add(cellp403);
-        cells.add(cellp404);
-        cells.add(cellp410);
-        cells.add(cellp411);
-        cells.add(cellp412);
-        cells.add(cellp413);
-        cells.add(cellp414);
-        cells.add(cellp420);
-        cells.add(cellp421);
-        cells.add(cellp422);
-        cells.add(cellp423);
-        cells.add(cellp424);
-        cells.add(cellp430);
-        cells.add(cellp431);
-        cells.add(cellp432);
-        cells.add(cellp433);
-        cells.add(cellp434);
-
     }
 
     @FXML
