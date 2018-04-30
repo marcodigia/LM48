51b3cfbd82f983efe38691716b3468a5e1d154d4
MattCrich <matthias.crichlow@gmail.com>
2018-05-01 01:11:32 +0200

    Player

--- src/main/java/it/polimi/ingsw/Client/CLI/CLI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/CLI.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Client.CLI;
 
+import it.polimi.ingsw.Client.View.UI;
 import it.polimi.ingsw.Exceptions.PlayersNumbersException;
 import it.polimi.ingsw.Server.Game.Cards.CardsComponents.Cell;
 import it.polimi.ingsw.Server.Game.Components.Boards.DraftPool;
@@ -7,13 +8,15 @@ import it.polimi.ingsw.Server.Game.Components.Dice;
 import it.polimi.ingsw.Server.Game.GameRules.Player;
 import it.polimi.ingsw.Server.Game.Utility.ANSI_COLOR;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //Decided to make CLI static , because it just need to print stuff
-public class CLI {
+public class CLI extends UI {
 
     private static final int maxNameSize = 18;
-    private static ArrayList<Player> players = new ArrayList<Player>();
+    private static ArrayList<Player> players = new ArrayList<>();
     private static int height = 4;
     private static DraftPool draftPool;
 
@@ -70,4 +73,62 @@ public class CLI {
         System.out.println();
     }
 
+    @Override
+    public int getDraftPoolIndex() {
+        System.out.println("Indice dado Draft pool : ");
+        InputStream is = System.in;
+        Scanner scanner = new Scanner(is);
+        int choice = scanner.nextInt();
+        if (choice >= 0 && choice < draftPool.getDraft().size())
+            return choice;
+        return -1;
+    }
+
+    @Override
+    public int getMatrixIndexFrom() {
+        System.out.println("Indice cella Window Pattern partenza : ");
+        InputStream is = System.in;
+        Scanner scanner = new Scanner(is);
+        int choice = scanner.nextInt();
+        if (choice >= 0 && choice < draftPool.getDraft().size())
+            return choice;
+        return -1;
+    }
+
+    @Override
+    public int getMatrixIndexTo() {
+        System.out.println("Indice cella window Pattern destinazione : ");
+        InputStream is = System.in;
+        Scanner scanner = new Scanner(is);
+        int choice = scanner.nextInt();
+        if (choice >= 0 && choice < draftPool.getDraft().size())
+            return choice;
+        return -1;
+    }
+
+    @Override
+    public int getRoundTrackIndex() {
+        System.out.println("Indice cella round Track : ");
+        InputStream is = System.in;
+        Scanner scanner = new Scanner(is);
+        int choice = scanner.nextInt();
+        if (choice >= 0 && choice < draftPool.getDraft().size())
+            return choice;
+        return -1;
+    }
+
+    @Override
+    public void UpdateDraftPol() {
+
+    }
+
+    @Override
+    public void updateWindowPattern(Player player) {
+
+    }
+
+    @Override
+    public void updateRoundTrack() {
+
+    }
 }
---
+++ src/main/java/it/polimi/ingsw/Server/Game/GameRules/Player.java
@@ -0,0 +1,81 @@
+package it.polimi.ingsw.Server.Game.GameRules;
+
+import it.polimi.ingsw.Client.View.UI;
+import it.polimi.ingsw.Client.View.UI_SIMULATION;
+import it.polimi.ingsw.Server.Game.Cards.CardsComponents.Cell;
+import it.polimi.ingsw.Server.Game.Cards.PrivateObjectiveCard;
+import it.polimi.ingsw.Server.Game.Cards.ToolCard;
+import it.polimi.ingsw.Server.Game.Cards.WindowPatternCard;
+import it.polimi.ingsw.Server.Game.Components.Boards.DraftPool;
+import it.polimi.ingsw.Server.Game.GameRules.Actions.Basic.TakeDiceBasic;
+import it.polimi.ingsw.Server.Game.GameRules.Actions.Basic.UseToolCardBasic;
+import it.polimi.ingsw.Server.Game.GameRules.PlayerUtility.PlayerColor;
+
+import java.util.ArrayList;
+
+//moved player in game rules so doAction is package friendly
+public class Player {
+
+
+    private WindowPatternCard windowPatternCard;
+    private PrivateObjectiveCard privateObjectiveCard;
+    private TakeDiceBasic placeDiceOfTheTurn;
+    private UseToolCardBasic useToolCardOfTheTurn;
+    private PlayerColor color;
+    private String name;
+    private DraftPool draftPool;
+    private UI ui;
+
+    public Player(PlayerColor color, String name) {
+        this.color = color;
+        this.name = name;
+        placeDiceOfTheTurn = new TakeDiceBasic(windowPatternCard, draftPool);
+
+        ui = new UI_SIMULATION();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PlayerColor getPlayerColor() {
+        return color;
+    }
+
+
+    public void setAction_placeDiceOfTheTurn(int from, int to) {
+        placeDiceOfTheTurn.takeDice(from, to);
+        placeDiceOfTheTurn.doAction();
+    }
+
+    public void setAction_UseToolCardOfTheTurn(ToolCard toolCard) {
+        useToolCardOfTheTurn = new UseToolCardBasic(toolCard, ui, draftPool, windowPatternCard);
+        useToolCardOfTheTurn.doAction();
+    }
+
+    public void endTurn() {
+        placeDiceOfTheTurn = null;
+        useToolCardOfTheTurn = null;
+    }
+
+
+    public ArrayList<Cell> getRow(int n) {
+        return windowPatternCard.getRow(n);
+    }
+
+    public WindowPatternCard getWindowPatternCard() {
+        return windowPatternCard;
+    }
+
+    public void setWindowPatternCard(WindowPatternCard windowPatternCard) {
+        this.windowPatternCard = windowPatternCard;
+    }
+
+    public DraftPool getDraftPool() {
+        return draftPool;
+    }
+
+    public void setDraftPool(DraftPool draftPool) {
+        this.draftPool = draftPool;
+    }
+}
