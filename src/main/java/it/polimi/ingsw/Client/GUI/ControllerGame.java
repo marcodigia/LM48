ab39b8843bb7a0818e3f43420101bae3f2d1174d
marco_dg <marco.digiacomatnio@outlook.it>
2018-05-05 23:23:39 +0200

    gameController with dices images

--- src/main/java/it/polimi/ingsw/Client/GUI/ControllerGame.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/ControllerGame.java
@@ -1,9 +1,16 @@
 package it.polimi.ingsw.Client.GUI;
 
+import it.polimi.ingsw.Server.Game.Cards.WindowPatternCard;
+import it.polimi.ingsw.Server.Game.Components.Boards.DraftPool;
+import it.polimi.ingsw.Server.Game.Components.Dice;
+import it.polimi.ingsw.Server.Game.Components.DiceBag;
+import it.polimi.ingsw.Server.Game.Utility.DiceColor;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -13,13 +20,19 @@ import static it.polimi.ingsw.Client.GUI.ControllerLogin.user;
 
 public class ControllerGame implements Initializable {
 
+    private int draftpoolindex = -1;
     private static Label draftToDisable;
     public static String diceToPut = "Empty";
+    public WindowPatternCard windowPatternCard;
+    public DraftPool draftPool;
+    ArrayList<Label> draftPoolLabel = new ArrayList<>();
+    ArrayList<String> pattern = new ArrayList<String>();
     public Label p4,
             cellp400, cellp401, cellp402, cellp403, cellp404,
             cellp410, cellp411, cellp412, cellp413, cellp414,
             cellp420, cellp421, cellp422, cellp423, cellp424,
-            cellp430, cellp431, cellp432, cellp433, cellp434;
+            cellp430, cellp431, cellp432, cellp433, cellp434,
+            draft1, draft2, draft3, draft4, draft5, draft6, draft7, draft8, draft9;
     private ArrayList<Label> cells = new ArrayList<>();
     private String buffer;
     private boolean put = false;
@@ -28,6 +41,9 @@ public class ControllerGame implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Game Window Loaded");
         p4.setText(user);
+
+        Dice dice = new Dice(DiceColor.RED, "4");
+
         cells.add(cellp400);
         cells.add(cellp401);
         cells.add(cellp402);
@@ -48,29 +64,45 @@ public class ControllerGame implements Initializable {
         cells.add(cellp432);
         cells.add(cellp433);
         cells.add(cellp434);
+        draftPoolLabel.add(draft1);
+        draftPoolLabel.add(draft2);
+        draftPoolLabel.add(draft3);
+        draftPoolLabel.add(draft4);
+        draftPoolLabel.add(draft5);
+        draftPoolLabel.add(draft6);
+        draftPoolLabel.add(draft7);
+        draftPoolLabel.add(draft8);
+        draftPoolLabel.add(draft9);
+
+        setUp();
+        setUpDP();
+        //updateDP();
+
+        for (int i = 0; i < 9; i++) {
+            draftPoolLabel.get(i).setGraphic(toImage(draftPool.getDraft().get(i)));
+        }
     }
 
     @FXML
     public void handleClickDraft(MouseEvent mouseEvent) {
-        //System.out.println("Draft");
         Label eventDraft = (Label) mouseEvent.getSource();
-        //System.out.println(eventDraft.getText());
         confirmDice(eventDraft);
-        buffer = eventDraft.getText();
+        draftpoolindex = draftPoolLabel.indexOf(eventDraft);
         draftToDisable = eventDraft;
         put = false;
     }
 
     @FXML
     public void handleClickWP(MouseEvent mouseEvent) {
-        diceToPut = buffer;
-        //System.out.println("WP");
         Label event = (Label) mouseEvent.getSource();
-        if (cells.contains(event) && !put && ((Label) mouseEvent.getSource()).getText().equals("Empty")) {
-            //System.out.println("WP in");
-            event.setText(diceToPut);
+        int indice_dado = cells.indexOf(event);
+        if (windowPatternCard.getDice(indice_dado)== null && !put) {
+            windowPatternCard.placeDice(draftPool.getDice(draftpoolindex), indice_dado, true,true,true);
+            event.setText("");
+            updateWP();
             draftToDisable.setDisable(true);
             put = true;
+            //updateDP();
         } else {
             createAlertBox();
         }
@@ -94,5 +126,59 @@ public class ControllerGame implements Initializable {
         String content = "You already placed this dice or in this cell has already been placed a dice! Please perform a correct move.";
         alert.setContentText(content);
         alert.showAndWait();
+    }
+
+    void setUp(){
+        pattern.add("2");
+        pattern.add("Via Lux");
+        pattern.add("4");
+        pattern.add("Y");   //0
+        pattern.add("0");   //1
+        pattern.add("6");   //2...
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("1");
+        pattern.add("5");
+        pattern.add("0");
+        pattern.add("2");
+        pattern.add("3");
+        pattern.add("Y");
+        pattern.add("R");
+        pattern.add("P");
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("0");
+        pattern.add("4");
+        pattern.add("3");
+        pattern.add("R");   //19
+        windowPatternCard = new WindowPatternCard(pattern);
+    }
+
+    void setUpDP(){
+        draftPool = new DraftPool(new DiceBag());
+        draftPool.extractNdice(9);
+    }
+
+    private void updateWP(){
+        for (int i=0; i<cells.size(); i++) {
+            if (windowPatternCard.getDice(i)!=null)
+                cells.get(i).setGraphic(toImage(windowPatternCard.getDice(i)));
+                //cells.get(i).setText(windowPatternCard.getDice(i).getValue() + windowPatternCard.getDice(i).getDiceColor());
+        }
+    }
+
+    /*private void updateDP(){
+        for (int i=0; i<draftPoolLabel.size(); i++) {
+            draftPoolLabel.get(i).setText(draftPool.getDice(i).getValue() + draftPool.getDice(i).getDiceColor());
+        }
+    }*/
+
+    private ImageView toImage(Dice dice){
+        Image image = new Image(dice.getDiceImage());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(30);
+        imageView.setFitWidth(30);
+        return imageView;
     }
 }
