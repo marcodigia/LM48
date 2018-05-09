5d875d51ed3e70d460b86da184632d6b9057c8e7
Fabiodallerive <37809568+fabiodallerive@users.noreply.github.com>
2018-05-09 18:58:37 +0200

    Network Structure

--- src/main/java/it/polimi/ingsw/Server/Game/GameRules/Game.java
+++ src/main/java/it/polimi/ingsw/Server/Game/GameRules/Game.java
@@ -1,18 +1,41 @@
 package it.polimi.ingsw.Server.Game.GameRules;
 
+
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Game {
 
-    private ArrayList<Player> players;
+    private Hashtable<Player ,Boolean> players;
     private GameContext gameContext;
 
     public Game(){
-        players = new ArrayList<>();
+        players = new Hashtable<>();
         //gameContext = new GameContext();
     }
-
+    //Aggiungi players alla partita
     public void addPlayer(ArrayList<Player> playersToAdd){
-        players.addAll(playersToAdd);
+        if(playersToAdd!=null)
+            for(Player p : playersToAdd){
+                players.put(p, true);
+                try {
+                    p.getServerClientSender().sendHello();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+    }
+    public boolean scanForPlayer(Player playerToFind){
+        return players.containsKey(playerToFind);
+    }
+    //Metti a false la partecipazione alla partita del player che si è disconesso
+    public boolean setPlayerToDisconnect(Player playerToDisconnect){
+        if(scanForPlayer(playerToDisconnect)) {
+            players.replace(playerToDisconnect, false);
+            return true;
+        }
+        return false;
     }
 }
