5aafaddc5bf491be65bb698359275bf304bf987f
MattCrich <matthias.crichlow@gmail.com>
2018-05-15 14:04:35 +0200

    Added Unpacker

---
+++ src/main/java/it/polimi/ingsw/Server/Game/Utility/Unpacker.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.Server.Game.Utility;
+
+import it.polimi.ingsw.Packetable;
+import it.polimi.ingsw.Server.Game.Cards.WindowPatternCard;
+
+import java.util.ArrayList;
+
+public class Unpacker {
+
+    WindowPatternCard WP_FromPacket(String packet) {
+
+        WindowPatternCard windowPatternCard = new WindowPatternCard(null);
+        return windowPatternCard;
+    }
+}
