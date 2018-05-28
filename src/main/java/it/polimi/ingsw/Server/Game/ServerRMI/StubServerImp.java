6fcafe2813e8e3d0f08999a5757df955444499a6
fabio <dallerivef@gmail.com>
2018-05-28 16:12:22 +0200

    Same fix action also for RMI

--- src/main/java/it/polimi/ingsw/Server/Game/ServerRMI/StubServerImp.java
+++ src/main/java/it/polimi/ingsw/Server/Game/ServerRMI/StubServerImp.java
@@ -3,8 +3,11 @@ package it.polimi.ingsw.Server.Game.ServerRMI;
 import it.polimi.ingsw.ClientServerCommonInterface.RMICommonInterface.StubServer;
 import it.polimi.ingsw.ClientServerCommonInterface.ServerClientSender;
 import it.polimi.ingsw.Server.Game.GameRules.Actions.Actions;
+import it.polimi.ingsw.Server.Game.GameRules.Actions.Basic.PlaceDiceAction;
+import it.polimi.ingsw.Server.Game.GameRules.Actions.Basic.UseToolCardBasic;
 import it.polimi.ingsw.Server.Game.GameRules.Player;
 import it.polimi.ingsw.Server.Game.ServerRete.Game;
+import it.polimi.ingsw.Server.Game.Utility.Unpacker;
 import it.polimi.ingsw.Server.Game.WaitingRoom.WaitingRoom;
 
 import java.rmi.RemoteException;
@@ -33,12 +36,18 @@ public class StubServerImp extends UnicastRemoteObject implements StubServer{
 
     @Override
     public void choosenWindowPattern(String id, String username) throws RemoteException {
+
         game.setWindowToPlayer(id,username);
         System.out.println(id + username);
     }
 
     @Override
     public void sendAction(Actions action, String username) throws RemoteException {
+        if (action instanceof PlaceDiceAction)
+            game.getGameStatus().getPlayerByName(username).setPlaceDiceOfTheTurn( (PlaceDiceAction) action) ;
+        else
+            game.getGameStatus().getPlayerByName(username).setUseToolCardOfTheTurn( (UseToolCardBasic) action); ;
+
         action.doAction(game.getGameStatus());
         for(Player p : game.getPlayers().keySet()) {
             p.getvirtualView().sendGameStatus(game.getGameStatus());
