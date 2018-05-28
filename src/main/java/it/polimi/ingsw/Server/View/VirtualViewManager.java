15b59cad4489cee9b2ab1d85cdff02f4b67c89d4
MattCrich <matthias.crichlow@gmail.com>
2018-05-28 10:36:53 +0200

    added VirtualViewManger

---
+++ src/main/java/it/polimi/ingsw/Server/View/VirtualViewManager.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.Server.View;
+
+import it.polimi.ingsw.Server.Game.GameRules.Player;
+
+import java.util.Hashtable;
+
+public class VirtualViewManager  {
+
+    static Hashtable<Player,VirtualView> virtualviews= new Hashtable<>();
+
+    public static synchronized void addVirtualView(Player player,VirtualView vv){
+        virtualviews.put(player,vv);
+    }
+
+    public static synchronized VirtualView getVirtualView(Player player){
+        return virtualviews.get(player);
+    }
+}
