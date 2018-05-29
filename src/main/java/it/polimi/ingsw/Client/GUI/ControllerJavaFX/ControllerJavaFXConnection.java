862affa75eecac2c265ee1e8fa81f7d9842585dc
marco_dg <marco.digiacomatnio@outlook.it>
2018-05-29 18:05:09 +0200

    rmi / socket gui

--- src/main/java/it/polimi/ingsw/Client/GUI/ControllerJavaFX/ControllerJavaFXConnection.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/ControllerJavaFX/ControllerJavaFXConnection.java
@@ -22,6 +22,7 @@ public class ControllerJavaFXConnection extends GUI implements Initializable {
     public Button rmibutton, socketbutton;
     public AnchorPane anchorconnection;
     public ImageView bg2;
+    public TextField serverIP, serverPort;
     public static ClientServerReciver clientServerReciver;
 
     @Override
